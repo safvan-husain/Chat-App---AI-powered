4c34cc4fb308eb5c9bb94119e1ba7ee0cfd98949
safvan-husain <safvanhusain009@gmail.com>
2023-02-10 19:22:39 +0530

    lvl-2

--- client/lib/local_database/message_services.dart
+++ client/lib/local_database/message_services.dart
@@ -1,11 +1,17 @@
 import 'package:client/local_database/message_schema.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 
 void deleteChatOf(String username, BuildContext context) async {
   var database = Provider.of<AppDatabase>(context, listen: false);
   await (database.delete(database.messages)
-        ..where((tbl) => tbl.senderId.equals(username)))
+        ..where((tbl) {
+          return tbl.senderId.equals(username);
+        }))
+      .go();
+  await (database.delete(database.messages)
+        ..where((tbl) {
+          return tbl.receiverId.equals(username);
+        }))
       .go();
 }
